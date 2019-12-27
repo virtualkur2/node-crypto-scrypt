81758302f750311adf264d1944647469ff6d83fd
Mauricio Contreras <virtual.kur2@gmail.com>
2019-12-27 21:01:21 +0100

    refactored hashedPassword format and reordering of arguments

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 const hashHelper = require('./helpers/hash.helper');
 
-const password = process.argv[2] || 'myV3ryS3c5r3P4ssw0rd';
-const method = (process.argv[3] ? process.argv[3].toLowerCase() : process.argv[3]) || 'async';
+const method = process.argv[2] ? process.argv[2].toLowerCase() : '';
+const password = process.argv[3] || 'myV3ryS3c5r3P4ssw0rd';
 
 //Using async:
 const usingAsync = async (pass) => {
   try {
     const hashed = await hashHelper.hashPassword(pass);
+    const valid = await hashHelper.isPasswordCorrect(pass,hashed);
     printHash(hashed);
+    printHashValidation(valid);
+    printTime();
   } catch(error) {
     printError(error);
   }
 }
 
 // Using then:
-const usingThen = (pass, method) => {
+const usingThen = (pass) => {
   hashHelper.hashPassword(pass)
     .then((hashed) => {
       printHash(hashed);
+      return hashHelper.isPasswordCorrect(pass, hashed);
+    })
+    .then((valid) => {
+      printHashValidation(valid);
+      printTime();
     })
     .catch((error) => {
       printError(error);
@@ -32,20 +40,36 @@ const usingCallback = (pass) => {
       return printError(error);
     }
     printHash(hashed);
+    hashHelper.isPasswordCorrect(pass, hashed, (err, valid) => {
+      if(err) {
+        return printError(err);
+      }
+      printHashValidation(valid);
+      printTime();
+    });
   });
 }
+
+//Auxiliar methods
 
 const printHash = (result) => {
   console.log(`El método usado es: ${method}`);
   console.log(`El password introducido es: ${password}`);
   console.log(`El hash generado es: ${result}`);
-  const end_time = new Date(Date.now()).getTime();
-  console.log(`El tiempo empleado fue: ${(end_time - start_time) / 1000} segundos`);
 }
 
 const printError = (error) => {
   console.log(`Hubo un error al procesar la solicitud:`);
   console.log(error.message);
+}
+
+const printHashValidation = (valid) => {
+  console.log(`¿El password introducido es válido? ${valid}`);
+}
+
+const printTime = () => {
+  const end_time = new Date(Date.now()).getTime();
+  console.log(`El tiempo empleado fue: ${(end_time - start_time) / 1000} segundos`);
 }
 
 const start_time = new Date(Date.now()).getTime();
